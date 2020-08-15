198a36d45a19e6bfedac2be4e74dd791e74783ef
Alex Rosso <alexrosso1025@gmail.com>
2020-08-14 22:18:27 -0500

    first commit

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+    <!--BOOTSTRAP 4-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <!--FONTAWESOME 6-->
+    <script src="https://kit.fontawesome.com/7d44b7b22a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">USERS</a>
+        </div>
+    </nav>
+    
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include("includes/header.php")?>
+    <div class="title">
+        <h1>Please Login or SignUp</h1>
+    </div>
+    <div class="container">
+        <a href="login.php">Login</a>
+        <a href="signup.php">SignUp</a>
+    </div>
+<?php include("includes/footer.php")?>
